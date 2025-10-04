4c84e224dbfd78e8b46a48cd90f93482947105db
Animainmilol <animainmilol@gmail.com>
2025-10-04 11:15:36 +0500

    yes

---
+++ camera.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+const (
+	MaxZoom float32 = 10.0
+	MinZoom float32 = 0.8
+
+	DefaultZoom          float32 = 1.0
+	DefaultRotation      float32 = 0.0
+	DefaultRotationSpeed float32 = 5.0
+	DefaultZoomSpeed     float32 = 5.0
+	DefaultFollowSpeed   float32 = 5.0
+)
+
+type CameraController struct {
+	camera rl.Camera2D
+
+	activeEffects []*CameraEffect
+
+	RotationSpeed float32
+	ZoomSpeed     float32
+	FollowSpeed   float32
+
+	followTarget rl.Vector2
+}
+
+type CameraEffect struct {
+	Name       string
+	ZoomFactor float32
+	Rotation   float32
+}
+
+func NewCameraController() *CameraController {
+	offsetX := rl.GetRenderWidth() / 2.0
+	offsetY := rl.GetRenderHeight() / 2.0
+
+	baseEffect := &CameraEffect{
+		Name:       "UserControl",
+		ZoomFactor: DefaultZoom,
+		Rotation:   DefaultRotation,
+	}
+
+	return &CameraController{
+		camera: rl.NewCamera2D(
+			rl.Vector2{X: float32(offsetX), Y: float32(offsetY)},
+			rl.Vector2{X: 0, Y: 0},
+			DefaultRotation,
+			DefaultZoom,
+		),
+
+		activeEffects: []*CameraEffect{baseEffect},
+
+		RotationSpeed: DefaultRotationSpeed,
+		ZoomSpeed:     DefaultZoomSpeed,
+		FollowSpeed:   DefaultFollowSpeed,
+
+		followTarget: rl.Vector2{X: 0, Y: 0},
+	}
+}
+
+func NewCameraEffect(name string, zoomFactor, rotation float32) *CameraEffect {
+	return &CameraEffect{
+		Name:       name,
+		ZoomFactor: zoomFactor,
+		Rotation:   rotation,
+	}
+}
+
+func (cc *CameraController) AddEffect(ce *CameraEffect) {
+	for _, effect := range cc.activeEffects {
+		if effect.Name == ce.Name {
+			return
+		}
+	}
+	cc.activeEffects = append(cc.activeEffects, ce)
+}
+
+func (cc *CameraController) RemoveEffect(name string) {
+	for i, effect := range cc.activeEffects {
+		if effect.Name == name {
+			lastIndex := len(cc.activeEffects) - 1
+			cc.activeEffects[i] = cc.activeEffects[lastIndex]
+			cc.activeEffects = cc.activeEffects[:lastIndex]
+			return
+		}
+	}
+}
+
+func (cc *CameraController) GetCamera() rl.Camera2D {
+	return cc.camera
+}
+
+func (cc *CameraController) SetFollowTarget(target rl.Vector2) {
+	cc.followTarget = target
+}
+
+func (cc *CameraController) Update() {
+	targetZoom := DefaultZoom
+	targetRotation := DefaultRotation
+
+	for _, v := range cc.activeEffects {
+		targetZoom *= v.ZoomFactor
+		targetRotation += v.Rotation
+	}
+
+	targetZoom = rl.Clamp(targetZoom, MinZoom, MaxZoom)
+
+	cc.camera.Zoom = rl.Lerp(cc.camera.Zoom, targetZoom, cc.ZoomSpeed*rl.GetFrameTime())
+	cc.camera.Rotation = rl.Lerp(cc.camera.Rotation, targetRotation, cc.RotationSpeed*rl.GetFrameTime())
+	cc.camera.Target = rl.Vector2Lerp(cc.camera.Target, cc.followTarget, cc.FollowSpeed*rl.GetFrameTime())
+}
--- main.go
+++ main.go
@@ -8,97 +8,11 @@ const (
 	DefaultScreenWidth  = 800
 	DefaultScreenHeight = 450
 	TargetFPS           = 120
-
-	MaxZoom = 10.0
-	MinZoom = 0.8
-
-	DefaultZoom          = 1.0
-	DefaultRotation      = 0.0
-	DefaultRotationSpeed = 5.0
-	DefaultZoomSpeed     = 5.0
-	DefaultFollowSpeed   = 5.0
 )
-
-type CameraController struct {
-	camera rl.Camera2D
-
-	ActiveEffects []CameraEffect
-
-	RotationSpeed float32
-	ZoomSpeed     float32
-	FollowSpeed   float32
-
-	followTarget rl.Vector2
-}
-
-type CameraEffect struct {
-	Name string
-	ZoomFactor float32
-	Rotation float32
-}
-
-func NewCameraController() *CameraController {
-	offsetX := float32(DefaultScreenWidth) / 2
-	offsetY := float32(DefaultScreenHeight) / 2
-
-	baseEffect := CameraEffect{
-        Name: "UserControl",
-        ZoomFactor: DefaultZoom,
-        Rotation: DefaultRotation,
-    }
-
-	return &CameraController{
-		camera: rl.NewCamera2D(
-			rl.Vector2{X: offsetX, Y: offsetY},
-			rl.Vector2{X: 0, Y: 0},
-			DefaultRotation,
-			DefaultZoom,
-		),
-
-		ActiveEffects: []CameraEffect{baseEffect},
-
-		RotationSpeed: DefaultRotationSpeed,
-		ZoomSpeed:     DefaultZoomSpeed,
-		FollowSpeed:   DefaultFollowSpeed,
-
-		followTarget: rl.Vector2{X: 0, Y: 0},
-	}
-}
-
-func NewCameraEffect(name string, zoomFactor, rotation float32) *CameraEffect {
-	return &CameraEffect{
-		Name: name,
-		ZoomFactor: zoomFactor,
-		Rotation: rotation,
-	}
-}
-
-func (cc *CameraController) GetCamera() rl.Camera2D {
-	return cc.camera
-}
-
-func (cc *CameraController) SetFollowTarget(target rl.Vector2) {
-    cc.followTarget = target
-}
-
-func (cc *CameraController) Update() {
-	var targetZoom float32
-	var targetRotation float32
-
-	for _, v := range cc.ActiveEffects {
-		targetZoom *= v.ZoomFactor
-	}
-	for _, v := range cc.ActiveEffects {
-		targetRotation += v.Rotation
-	}
-
-	cc.camera.Zoom = rl.Lerp(cc.camera.Zoom, targetZoom, cc.ZoomSpeed*rl.GetFrameTime())
-	cc.camera.Rotation = rl.Lerp(cc.camera.Rotation, targetRotation, cc.RotationSpeed*rl.GetFrameTime())
-	cc.camera.Target = rl.Vector2Lerp(cc.camera.Target, cc.followTarget, cc.FollowSpeed*rl.GetFrameTime())
-}
 
 func main() {
 	rl.InitWindow(DefaultScreenWidth, DefaultScreenHeight, "RGWTWC")
+	rl.SetConfigFlags(rl.FlagWindowResizable)
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(TargetFPS)
@@ -106,9 +20,10 @@ func main() {
 	cc := NewCameraController()
 
 	for !rl.WindowShouldClose() {
+		cc.Update()
+
 		rl.BeginDrawing()
 
-		cc.Update()
 		rl.BeginMode2D(cc.GetCamera())
 
 		rl.ClearBackground(rl.Black)
